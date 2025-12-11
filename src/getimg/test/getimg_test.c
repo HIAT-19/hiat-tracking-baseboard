5a218ea1cbcda54b44a72327806bed6fa8ff9953
dzxw-xw <884377698@qq.com>
2025-12-11 16:35:48 +0800

    调整结构体，分离设备相关代码初始化

--- src/getimg/test/getimg_test.c
+++ src/getimg/test/getimg_test.c
@@ -1,26 +1,66 @@
 
 #include "img_get.h"
-
-int main(int argc, char** argv) 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+void* readThread0(void* arg)
 {
-	int i;
+	unsigned char *img;
 	ImgGet* imgdev;
 	ImgGetInit(&imgdev, "xdma218", "/home/xdma.ko", 1080, 1920, 0, 36 , 3);
-	//ImgGetInit(&imgdev, "xdma218", "/home/xdma.ko", 1080, 1920, 1, 8 , 3);
-	unsigned char *img;
 	
-
+	int i = 0;
+	FILE* fp;
+	char cmdstr[100] = {0};
+	int ret;
 	while(1){
-		ImgGetRead(imgdev->ctx, &img);
+		ret = ImgGetRead(imgdev->ctx, &img);
+		memset(cmdstr, 0, 100);
+		sprintf(cmdstr,"a%d.raw",i);
+		fp = fopen(cmdstr,"w+b");
+		fwrite(img,1,1920*1080*2,fp);
+		fclose(fp);
+		printf("readThread0: ret = %d\n", ret);
 		ImgPut(imgdev->ctx);
+		i++;
+	}
+
+    return NULL;
+}
+void* readThread1(void* arg)
+{
+	unsigned char *img;
+	ImgGet* imgdev;
+	ImgGetInit(&imgdev, "xdma218", "/home/xdma.ko", 1080, 1920, 1, 8 , 3);
+	int i = 0;
+	FILE* fp;
+	char cmdstr[100] = {0};
+	int ret;
+	while(1){
+		ret = ImgGetRead(imgdev->ctx, &img);
+		memset(cmdstr, 0, 100);
+		sprintf(cmdstr,"b%d.raw",i);
+		fp = fopen(cmdstr,"w+b");
+		fwrite(img,1,1920*1080*2,fp);
+		fclose(fp);
+		printf("readThread1: ret = %d\n", ret);
+		ImgPut(imgdev->ctx);
+		i++;
 	}
 	
+    return NULL;
+}
+int main(int argc, char** argv) 
+{
+	pthread_t threadRead;
+	pthread_t threadRead1;
+	pthread_create(&threadRead, NULL, readThread0, NULL);
+	pthread_create(&threadRead1, NULL, readThread1, NULL);
 
-	FILE* fp;
-	fp = fopen("b.raw","w+b");
-	fwrite(img,1,1920*1080*2,fp);
-	fclose(fp);
-
+    pthread_join(threadRead1, NULL);	
+    pthread_join(threadRead, NULL);
     return 0;
 }
 
